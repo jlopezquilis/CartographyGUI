fbc0cf27979942f113f066f2a43f6d15c94ed3fd
Peeybol <Peeybol@users.noreply.github.com>
2022-05-19 01:30:09 +0200

    pantalla d preguntas

---
+++ src/poiupv/ExercisesController.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poiupv;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+
+/**
+ * FXML Controller class
+ *
+ * @author peybo
+ */
+public class ExercisesController implements Initializable {
+
+    @FXML
+    private RadioButton answer1;
+    @FXML
+    private ToggleGroup answers;
+    @FXML
+    private RadioButton answer2;
+    @FXML
+    private RadioButton answer3;
+    @FXML
+    private RadioButton answer4;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
